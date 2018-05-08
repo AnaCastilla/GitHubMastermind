bb31a647637212de16de85607e01a8af590f608d
Unknown <ana.castilla1998@gmail.com>
2018-05-08 11:04:55 +0200

    Probando 3

--- src/Test/MastermindTest.java
+++ src/Test/MastermindTest.java
@@ -1,5 +1,7 @@
 package Test;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -22,25 +24,73 @@ class MastermindTest {
 	void respuestaTest() {
 		
 	
-		System.out.println("CLASES DE EQUIVALENCIA VÁLIDAS\nTodas las fichas están bien colocadas:\nModo Fácil: ");
+		System.out.println("CLASES DE EQUIVALENCIA VÁLIDAS\nTodas las fichas están bien colocadas -> las fichas de la respuesta son "
+				+ "las mismas que la de la combinación secreta:\nModo Fácil: ");
 		assertArrayEquals(Combinacion.respuesta(ModoDeJuego.FACIL), Combinacion.combSecreta(ModoDeJuego.FACIL));
 		System.out.println("Modo Medio: ");
 		assertArrayEquals(Combinacion.respuesta(ModoDeJuego.MEDIO), Combinacion.combSecreta(ModoDeJuego.MEDIO));
 		System.out.println("Modo Difícil: ");
 		assertArrayEquals(Combinacion.respuesta(ModoDeJuego.DIFICIL), Combinacion.combSecreta(ModoDeJuego.DIFICIL));
 		
-		System.out.println("\nNinguna ficha bien colocada: \nModo Fácil: ");
+		System.out.println("\nNinguna ficha bien colocada -> todas las fichas de la respuesta son diferentes de las de la"
+				+ "combinación secreta: \nModo Fácil: ");
 		assertNotEquals(Combinacion.respuesta(ModoDeJuego.FACIL), Combinacion.combSecreta(ModoDeJuego.FACIL));
 		System.out.println("Modo Medio: ");
 		assertNotEquals(Combinacion.respuesta(ModoDeJuego.MEDIO), Combinacion.combSecreta(ModoDeJuego.MEDIO));
+		System.out.println("Modo Difícil: ");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.DIFICIL), Combinacion.combSecreta(ModoDeJuego.DIFICIL));
+		
+		System.out.println("Distintas respuestas que se pueden dar dependiendo de la posición de las fichas (modo fácil)\n"
+				+ "Introduce 3 colores iguales y 1 distinto");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.FACIL), Combinacion.combSecreta(ModoDeJuego.FACIL));
+		System.out.println("introduce 2 colores iguales y 2 distintos");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.FACIL), Combinacion.combSecreta(ModoDeJuego.FACIL));
+		System.out.println("introduce 1 color igual y 3 distintos");
+		
+		System.out.println("Colores repetidos en el modo difícil\n Escribe 8 colores para el modo difícil que se repitan:");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.DIFICIL), Combinacion.combSecreta(ModoDeJuego.DIFICIL));
+		
+		System.out.println("CLASES DE EQUIVALENCIA NO VÁLIDAS\nNingún color de la respuesta está en la combinación secreta\nModo Fácil: ");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.FACIL), Combinacion.combSecreta(ModoDeJuego.FACIL));
+		System.out.println("Modo Medio: ");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.MEDIO), Combinacion.combSecreta(ModoDeJuego.MEDIO));
+		System.out.println("Modo Difícil (se pueden repetir colores): ");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.DIFICIL), Combinacion.combSecreta(ModoDeJuego.DIFICIL));
+		
+		System.out.println("Se repiten colores cuando no se puede\nModo Fácil:");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.FACIL), Combinacion.combSecreta(ModoDeJuego.FACIL));
+		System.out.println("Modo Medio: ");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.MEDIO), Combinacion.combSecreta(ModoDeJuego.MEDIO));
+
+		System.out.println("Combinación secreta con valores nulos -> introduce un espacio en blanco o null:\nModo Fácil");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.FACIL), Combinacion.combSecreta(ModoDeJuego.FACIL));
+		System.out.println("Modo Medio: ");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.MEDIO), Combinacion.combSecreta(ModoDeJuego.MEDIO));
+		System.out.println("Modo Difícil (se pueden repetir colores): ");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.DIFICIL), Combinacion.combSecreta(ModoDeJuego.DIFICIL));
+		
+		System.out.println("Respuesta con valores nulos -> introduce un espacio en blanco o null:\nModo Fácil");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.FACIL), Combinacion.combSecreta(ModoDeJuego.FACIL));
+		System.out.println("Modo Medio: ");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.MEDIO), Combinacion.combSecreta(ModoDeJuego.MEDIO));
+		System.out.println("Modo Difícil: ");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.DIFICIL), Combinacion.combSecreta(ModoDeJuego.DIFICIL));
+		
+		System.out.println("No existe combinación secreta -> no se introduce nada en la combinación secreta:\nModo Fácil:");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.FACIL), Combinacion.combSecreta(ModoDeJuego.FACIL));
+		System.out.println("Modo Medio: ");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.MEDIO), Combinacion.combSecreta(ModoDeJuego.MEDIO));
+		System.out.println("Modo Difícil: ");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.DIFICIL), Combinacion.combSecreta(ModoDeJuego.DIFICIL));
+		
+		System.out.println("No existe respuesta -> no se introduce nada en la respuesta\nModo Fácil:");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.FACIL), Combinacion.combSecreta(ModoDeJuego.FACIL));
+		System.out.println("Modo Medio: ");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.MEDIO), Combinacion.combSecreta(ModoDeJuego.MEDIO));
+		System.out.println("Modo Difícil: ");
+		assertNotEquals(Combinacion.respuesta(ModoDeJuego.DIFICIL), Combinacion.combSecreta(ModoDeJuego.DIFICIL));
 		
 		
-		
-		
-		
-		//System.out.println("Ningún color de la respuesta está en la combinación secreta (modo medio, porque no se pueden repetir colores)");
-		
-		//assertArrayEquals(Combinacion.respuesta(ModoDeJuego.MEDIO), Combinacion.combSecreta(ModoDeJuego.MEDIO));
 	}
 
 }
